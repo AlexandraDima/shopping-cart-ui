98c57c263a81b394776e71f04b272f7af63c4239
Alexandra Dima <alexandra.dima89@yahoo.com>
2020-08-24 11:30:06 +0200

    Updates

---
+++ precache-manifest.fcd545d5b217a911bd067c8e17dca7bb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae7cd6d21747269698d231a44f4c09c",
+    "url": "/shopping-cart-ui/index.html"
+  },
+  {
+    "revision": "6f6f413584c490a4e45e",
+    "url": "/shopping-cart-ui/static/css/2.868afef0.chunk.css"
+  },
+  {
+    "revision": "c0c2c6e1649ee77403fa",
+    "url": "/shopping-cart-ui/static/css/main.9121fc65.chunk.css"
+  },
+  {
+    "revision": "6f6f413584c490a4e45e",
+    "url": "/shopping-cart-ui/static/js/2.705c2b23.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/shopping-cart-ui/static/js/2.705c2b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c2c6e1649ee77403fa",
+    "url": "/shopping-cart-ui/static/js/main.13f133c4.chunk.js"
+  },
+  {
+    "revision": "02152190d9273b80136a",
+    "url": "/shopping-cart-ui/static/js/runtime-main.c79b8aff.js"
+  },
+  {
+    "revision": "33ef3952243bd9f20f8821a829620bfd",
+    "url": "/shopping-cart-ui/static/media/Cargo_Train.33ef3952.png"
+  },
+  {
+    "revision": "5c788faa2f03f6c421e0b1dd3d38ddac",
+    "url": "/shopping-cart-ui/static/media/Creative_animals.5c788faa.png"
+  },
+  {
+    "revision": "6d41d69173eb23dfd3449f921869d038",
+    "url": "/shopping-cart-ui/static/media/Modular_Playhouse.6d41d691.png"
+  },
+  {
+    "revision": "08941e520055e4a61af08709b3740610",
+    "url": "/shopping-cart-ui/static/media/Tower_Crane_Construction.08941e52.png"
+  },
+  {
+    "revision": "f6e4f1bba6d8eb7d163680d0b8909f1c",
+    "url": "/shopping-cart-ui/static/media/World_Animals.f6e4f1bb.png"
+  },
+  {
+    "revision": "5a530721c96213b6a735fa7c74535cf9",
+    "url": "/shopping-cart-ui/static/media/Wrecking_Ball_Demolition.5a530721.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shopping-cart-ui/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shopping-cart-ui/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shopping-cart-ui/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shopping-cart-ui/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shopping-cart-ui/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
